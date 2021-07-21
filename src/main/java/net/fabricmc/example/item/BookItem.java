29df1ea12463fe44693b2be443834d87d8b115e3
ti-ji <1250079251@qq.com>
2021-07-21 16:49:41 +0800

    Item 事件

--- src/main/java/net/fabricmc/example/item/BookItem.java
+++ src/main/java/net/fabricmc/example/item/BookItem.java
@@ -1,9 +1,12 @@
 package net.fabricmc.example.item;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.StackReference;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.screen.slot.Slot;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ClickType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -21,4 +24,18 @@ public class BookItem extends Item {
 		return TypedActionResult.success(playerEntity.getStackInHand(hand));
 	}
 
+	@Override
+	public boolean onClicked(ItemStack stack, ItemStack otherStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference) {
+		return super.onClicked(stack, otherStack, slot, clickType, player, cursorStackReference);
+	}
+
+	@Override
+	public void onCraft(ItemStack stack, World world, PlayerEntity player) {
+		super.onCraft(stack, world, player);
+	}
+
+	@Override
+	public boolean onStackClicked(ItemStack stack, Slot slot, ClickType clickType, PlayerEntity player) {
+		return super.onStackClicked(stack, slot, clickType, player);
+	}
 }
